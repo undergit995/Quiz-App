497a5a5fb10530bcb9da874e98f77a4d1527c80d
undergit995 <avinash.avi7a@gmail.com>
2026-06-10 09:15:46 +0530

    v12

--- src/Pages/LandingPage/Home.jsx
+++ src/Pages/LandingPage/Home.jsx
@@ -11,7 +11,6 @@ export default function Home() {
         <Box sx={{height:800,width:443,bgcolor:'tomato',position:'absolute',left:-201,top:-100,zIndex:-9,transform:'rotate(18deg)'}}/>
       <Box sx={{display:'flex',width:'100vw',position:'relative',py:5,mt:10}}>
         <Box sx={{borderRadius:3,m:13,backgroundColor:theme.colorSchemes.light.palette.primary.main,width:443,p:4,transform:'rotate(18deg)'}}>
-          <Typography variant="h5" color="initial">Tests</Typography>
           <Typography variant="h6" color="initial">Compete with your friends</Typography>
           <Typography variant="h5" color="initial">Test your skills now</Typography>
           <Typography variant="h5" color="initial">Practice makes perfect</Typography>
